2c9bb311ae601388fa5924da71136fe2586c3ae6
TodorCNC <todorcnc@gmail.com>
2022-03-08 18:17:18 +0100

    Added Serializers and URLS to the project

---
+++ test_project/test_project/companies/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+
+from .views import CompanyViewSet
+
+companies_router = routers.DefaultRouter()
+companies_router.register("companies", viewset=CompanyViewSet, basename="companies")
+
+urlpatterns = companies_router.urls
--- test_project/test_project/companies/views.py
+++ test_project/test_project/companies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Company
+from .serializers import CompanySerializer
+
 # Create your views here.
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_update")
+    pagination_class = PageNumberPagination
